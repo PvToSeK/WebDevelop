dcb3f7032a91286da094ab3826d0c70669357305
diegomarinsek <marinseksamuel@gmail.com>
2025-11-27 13:42:54 +0100

    freeeeeeeeeeee

---
+++ AsyncJs/Esercizio1/script.js
@@ -0,0 +1,34 @@
+const URL = "https://jsonplaceholder.typicode.com/users/1";
+let resElem = document.getElementById("res");
+
+// NO ASYNC/AWAIT
+/*fetch(URL).then(response => {
+    console.log(response);
+    // response.ok === response.status === 200
+    if (response.ok) {
+        // parse body (.json() è async)
+        response.json().then(data => {
+            console.log(data);
+            // console.log(typeof data);
+            // resElem.textContent = JSON.stringify(data);
+            for (key in data) {
+                console.log(data[key]);
+            }
+        });
+    } else {
+        // tutto male
+        console.log("ERRORE: ", response.status);
+    }
+});
+*/
+fetch(URL)
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("ERRORE HTTP: " + response.status);
+    }
+  })
+  .then(data => console.log(data))
+  .catch(error => console.log(error));
+
