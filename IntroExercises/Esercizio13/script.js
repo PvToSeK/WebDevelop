7315ff11a853c4b062ce6997ae3148a7d0a1ec5e
diegomarinsek <marinseksamuel@gmail.com>
2025-11-30 16:18:55 +0100

    ds

--- IntroExercises/Esercizio13/script.js
+++ IntroExercises/Esercizio13/script.js
@@ -1,3 +1,20 @@
-let rosso
-let giallo
-let verde
+let colori = ["rosso","giallo","verde","giallo"]
+let indice = 0;
+
+
+let bottone = document.getElementById("avvia")
+
+function spegniTutte() {
+    document.getElementById("rosso").style.opacity = "0.3";
+    document.getElementById("giallo").style.opacity = "0.3";
+    document.getElementById("verde").style.opacity = "0.3";
+}
+
+function accendi(){
+    spegniTutte();
+     let luceCorrente = colori[indice];
+     document.getElementById(luceCorrente).style.opacity = "1";
+    indice++
+    indice = indice % colori.length;
+}
+bottone.addEventListener("click", accendi);
