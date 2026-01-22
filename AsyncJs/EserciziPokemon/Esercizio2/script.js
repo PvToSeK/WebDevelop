15e3bf9ff517862affe32d4aebcda9c1094b4567
diegomarinsek <marinseksamuel@gmail.com>
2026-01-22 23:22:14 +0100

    ok

--- AsyncJs/EserciziPokemon/Esercizio2/script.js
+++ AsyncJs/EserciziPokemon/Esercizio2/script.js
@@ -1,1 +1,7 @@
 const URL = "https://pokeapi.co/api/v2/pokemon/zoroark"
+
+
+fetch(URL).then(response => response.json()).then(data => {
+    let nome= data.forms[0].name;
+    console.log(nome)
+})
